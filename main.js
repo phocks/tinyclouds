969e3e65b3e22448477195bda62504d5e26945da
Joshua Byrd <phocks@gmail.com>
2023-01-10 11:00:45 +1000

    I don't think the bg is working

--- main.js
+++ main.js
@@ -3,13 +3,13 @@ import blog, { ga, redirects } from "https://deno.land/x/blog@0.5.0/blog.tsx";
 blog({
   title: "Joshua Byrd",
   author: "Joshua Byrd",
-  avatar: "./ry.jpg",
+  avatar: "./plant.jpg",
   avatarClass: "full",
   links: [
     { title: "Email", url: "mailto:phocks@gmail.com" },
     { title: "GitHub", url: "https://github.com/phocks" },
   ],
-  background: "#eee",
+  background: "#9ee",
   middlewares: [
     ga("UA-91675022-1"),
     redirects({
