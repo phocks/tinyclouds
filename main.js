26b35a0acdb6441a30a683949d4b8e4a18b224f7
Joshua Byrd <phocks@gmail.com>
2023-01-10 10:56:12 +1000

    Does this actually do anything?

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ blog({
     { title: "Email", url: "mailto:phocks@gmail.com" },
     { title: "GitHub", url: "https://github.com/phocks" },
   ],
-  background: "#efebe9",
+  background: "#eee",
   middlewares: [
     ga("UA-91675022-1"),
     redirects({
